4507c797d9c5576d0c656457655928897274f2d9
Antoine Bosselut <antoine.bosselut@gmail.com>
2017-02-24 13:25:13 -0800

    Added utils file

---
+++ utils/utils.lua
@@ -0,0 +1,48 @@
+local model_utils = {}
+
+
+function model_utils.copy_table(to, from)
+  assert(#to == #from)
+  for i = 1, #to do
+    to[i]:copy(from[i])
+  end
+end
+
+function model_utils.cloneManyTimes(net, T)
+  local clones = {}
+  local params, gradParams = net:parameters()
+  if params == nil then
+    params = {}
+  end
+  local paramsNoGrad
+  if net.parametersNoGrad then
+    paramsNoGrad = net:parametersNoGrad()
+  end
+  local mem = torch.MemoryFile("w"):binary()
+  mem:writeObject(net)
+  for t = 1, T do
+    -- We need to use a new reader for each clone.
+    -- We don't want to use the pointers to already read objects.
+    local reader = torch.MemoryFile(mem:storage(), "r"):binary()
+    local clone = reader:readObject()
+    reader:close()
+    local cloneParams, cloneGradParams = clone:parameters()
+    local cloneParamsNoGrad
+    for i = 1, #params do
+      cloneParams[i]:set(params[i])
+      cloneGradParams[i]:set(gradParams[i])
+    end
+    if paramsNoGrad then
+      cloneParamsNoGrad = clone:parametersNoGrad()
+      for i =1,#paramsNoGrad do
+        cloneParamsNoGrad[i]:set(paramsNoGrad[i])
+      end
+    end
+    clones[t] = clone
+    collectgarbage()
+  end
+  mem:close()
+  return clones
+end
+
+return model_utils
